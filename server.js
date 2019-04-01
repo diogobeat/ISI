285ceb52fe7c552bfcb82ee81a6dce0d8bacc03c
diogobeat <33163443+diogobeat@users.noreply.github.com>
2019-04-01 14:56:36 +0100

    Revert "Revert "hash""
    This reverts commit a65a47e5db9f96e7a89efde975a475e1b703fabc.

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mysql = require('mysql');
 const bodyParser = require('body-parser');
 const app = express();
 const validator = require('express-validator');
+global.bcrypt = require('bcrypt');
 
 
 
