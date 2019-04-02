f4fa1e3ea7d3526396254619b3599cbb00f3e0dc
Tiago Pimenta Da Costa <tiagopimcosta@gmail.com>
2019-04-02 04:48:14 +0100

    admin table sales order

--- controllers/admin.route.js
+++ controllers/admin.route.js
@@ -7,21 +7,19 @@ const reque = require('request');
 router.get('/', function(request,response){
 	//console.log(request.user);
 	//console.log(request.isAuthenticated());
-		reque.get('http://localhost:8091/jasminapi/getsalesorder', function(error,response2,body){
+		reque.get('http://localhost:8089/jasminapi/getsalesorder', function(error,response2,body){
 			var jasmin;
 			if(!error && response2.statusCode == 200){
 				jasmin = JSON.parse(body);
 				var soma = 0;
 				for(var i = 0 ; i < jasmin.root.data.length ; i++){
-					soma = soma + jasmin.root.data[i].grossValue.amount;
-					var val = [soma.length - 1];
-					console.log(soma);
+					soma += jasmin.root.data[i].amount;
 				}
 
 			}
 			response.set("Content-Type", "text/html");
 			response.render('admin/index', {body: jasmin,
-				soma : soma});
+											soma : soma});
 		});
 	
 	
