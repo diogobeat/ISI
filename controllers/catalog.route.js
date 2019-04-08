b020d755d7da239b5a4575e6c0184525450f2b44
diogobeat <33163443+diogobeat@users.noreply.github.com>
2019-04-08 23:20:22 +0100

    get items

--- controllers/catalog.route.js
+++ controllers/catalog.route.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const reque = require('request');
 
 
 router.get('/', function(request, response){
 	//console.log(request.user);
 	//console.log(request.isAuthenticated());
+	reque.get('http://localhost:8091/jasminapi/getItems', function(error,response2,body){
+		var jasmin = JSON.parse(body);
+		for ( var i = 0 ; i < jasmin.root.data.length; i ++){
+			console.log(jasmin.root.data[i].itemKey);
+		}
+		
 	response.set("Content-Type", "text/html");
-	response.render('catalog', {})
+	response.render('catalog', {body : jasmin})
+	})
 });
 
 module.exports = router;
