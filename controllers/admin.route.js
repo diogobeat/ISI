ff200b6e4ac53cd8f6c252e8a53cb2dac001c49f
diogobeat <33163443+diogobeat@users.noreply.github.com>
2019-04-21 23:29:21 +0100

    update tabelas

--- controllers/admin.route.js
+++ controllers/admin.route.js
@@ -7,7 +7,7 @@ const reque = require('request');
 router.get('/', global.secure("admin"),function(request,response){
 	//console.log(request.user);
 	//console.log(request.isAuthenticated());
-		reque.get('http://localhost:8089/jasminapi/getsalesorder', function(error,response2,body){
+		reque.get('http://localhost:8089/jasminapi/getOrder', function(error,response2,body){
 			var earningsMonthly = 0;
 			var earningsAnnual = 0;
 			var currentDate = new Date();
@@ -15,15 +15,15 @@ router.get('/', global.secure("admin"),function(request,response){
 			var month = (currentDate.getMonth() + 1).toString();
 
 			var jasmin = JSON.parse(body);
-			for(var i = 0 ; i < jasmin.root.data.length ; i++){
-				var saleM = jasmin.root.data[i].documentDate.substring(5, 7).replace(/^0+/, '');
-				var saleA = jasmin.root.data[i].documentDate.substring(0, 4);
+			for(var i = 0 ; i < jasmin.data.length ; i++){
+				var saleM = jasmin.data[i].documentDate.substring(5, 7).replace(/^0+/, '');
+				var saleA = jasmin.data[i].documentDate.substring(0, 4);
 				//console.log(saleA  == year);
 				if(saleA  === year){
-					earningsAnnual +=  jasmin.root.data[i].amount;
+					earningsAnnual +=  jasmin.data[i].amount;
 
 					if(saleM === month){
-						earningsMonthly += jasmin.root.data[i].amount;
+						earningsMonthly += jasmin.data[i].amount;
 					}
 				}		
 			}
