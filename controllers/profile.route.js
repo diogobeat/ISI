90b9c026096e4464f23e9c01162e4c498f70b0d2
diogobeat <33163443+diogobeat@users.noreply.github.com>
2019-04-29 00:15:50 +0100

    update index.ejs

--- controllers/profile.route.js
+++ controllers/profile.route.js
@@ -1,8 +1,30 @@
 const express = require('express');
 const router = express.Router();
 const usersModel = require('../models/user.model');
+const req = require('request');
+
+
+const headersOpt = {  
+	"content-type": "application/json",
+};
+
+function getClinte(){
+	var options = {
+		url : 'http://localhost:8091/jasmin/getClients',
+		method: 'GET',
+		dataType : 'json',
+		headers: headersOpt
+	}
+	req.get(options, function(body){
+	console.log(JSON.stringify(body));
+	});
+}
+
 
 router.get('/', global.secure(), function(request, response) {
+	req.get("http://localhost:8091/jasmin/getClients", function(body){
+		console.log(JSON.stringify(body));
+	});
 	response.set("Content-Type", "text/html");
 	response.render('profile', {
 		user: request.user, errors: []
