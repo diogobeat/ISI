f0caaac241b713820b7921c4194e57fb13ac30c0
NunoXavierFernandes <nunofxff@gmail.com>
2019-05-04 14:58:53 +0100

    job Moloni supplierOrder

--- controllers/catalog.route.js
+++ controllers/catalog.route.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const reque = require('request');
-
+const headersOpt = {  
+	"content-type": "application/json",
+};
 
 router.get('/', function(request, response){
 	//console.log(request.user);
@@ -16,5 +18,64 @@ router.get('/', function(request, response){
 	response.render('catalog', {body : jasmin})
 	})
 });
+
+
+
+router.get('/carrinho', function(request, response) {
+	response.set("Content-Type", "text/html");
+	response.render('carrinho', {
+		errors: []
+	})
+	
+});
+
+
+
+router.get('/buy', function(request, response) {
+	response.set("Content-Type", "text/html");
+	response.render('carrinho', {
+		errors: []
+	})
+	console.log('nabo');
+});
+	
+	
+router.post('/buy', function(request, response) {
+	
+	console.log('nabo2');
+
+			var options = {
+				url : 'http://localhost:8091/moloni/createSupplierOrder',
+				method: 'POST',
+				dataType : 'json',
+				headers: headersOpt,
+				json: {
+					"company_id": "72407",
+        "date": "2019-04-09T17:31:05+0100",
+        "expiration_date": "2019-05-09",
+        "document_set_id": "123228",
+        "supplier_id": "533438",
+        "json": true,
+        "products":  [{
+            "product_id": "22435961",
+            "name": "Balcão",
+            "qty": "1",
+            "price": "1000",
+            "exemption_reason": "M01"
+            }]
+			}
+						}
+			console.log(options);
+			
+			reque.post(options, function(error, response){
+				if(!error && response.statusCode == 200){
+					}
+				});			
+
+			response.redirect('/');
+
+});
+
+
 
 module.exports = router;
