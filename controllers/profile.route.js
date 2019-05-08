0af6bc07f90247998680e8aaf839487eed655042
diogobeat <33163443+diogobeat@users.noreply.github.com>
2019-05-08 01:03:09 +0100

    update profile

--- controllers/profile.route.js
+++ controllers/profile.route.js
@@ -8,21 +8,11 @@ const headersOpt = {
 	"content-type": "application/json",
 };
 
-function getClinte(){
-	var options = {
-		url : 'http://localhost:8091/jasmin/getClients',
-		method: 'GET',
-		dataType : 'json',
-		headers: headersOpt
-	}
-	req.get(options, function(body){
-	console.log(JSON.stringify(body));
-	});
-}
+
 
 
 router.get('/', global.secure(), function(request, response) {
-	req.get("http://localhost:8091/jasmin/getClients", function(body){
+	req.get("http://localhost:8091/jasmin/getClients/INDIF", function(body){
 		console.log(JSON.stringify(body));
 	});
 	response.set("Content-Type", "text/html");
