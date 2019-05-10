f6d74f78969304ab4877d91a9f99fcd986d1802b
NunoXavierFernandes <nunofxff@gmail.com>
2019-05-10 13:18:43 +0100

    jobs SupplierInvoice and SupplierReceipt

--- controllers/catalog.route.js
+++ controllers/catalog.route.js
@@ -31,18 +31,16 @@ router.get('/carrinho', function(request, response) {
 
 
 
-router.get('/buy', function(request, response) {
+router.get('/encomendar', function(request, response) {
 	response.set("Content-Type", "text/html");
 	response.render('carrinho', {
 		errors: []
 	})
-	console.log('nabo');
 });
 	
 	
-router.post('/buy', function(request, response) {
+router.post('/encomendar', function(request, response) {
 	
-	console.log('nabo2');
 
 			var options = {
 				url : 'http://localhost:8091/moloni/createSupplierOrder',
@@ -55,7 +53,6 @@ router.post('/buy', function(request, response) {
         "expiration_date": "2019-05-09",
         "document_set_id": "123228",
         "supplier_id": "533438",
-        "json": true,
         "products":  [{
             "product_id": "22435961",
             "name": "Balcão",
@@ -65,7 +62,6 @@ router.post('/buy', function(request, response) {
             }]
 			}
 						}
-			console.log(options);
 			
 			reque.post(options, function(error, response){
 				if(!error && response.statusCode == 200){
@@ -73,6 +69,91 @@ router.post('/buy', function(request, response) {
 				});			
 
 			response.redirect('/');
+
+});
+
+
+
+router.get('/buy', function(request, response) {
+	response.set("Content-Type", "text/html");
+	response.render('carrinho', {
+		errors: []
+	})
+});
+	
+	
+router.post('/buy', function(request, response) {
+	
+
+			var options = {
+				url : 'http://localhost:8091/moloni/createSupplierInvoice',
+				method: 'POST',
+				dataType : 'json',
+				headers: headersOpt,
+				json: {
+					"company_id": "72407",
+					"date": "2019-04-09T17:31:05+0100",
+					"document_set_id": "123228",
+					"supplier_id": "533438",
+					"associated_documents": [{
+						"associated_id": "0",
+						"value": "0"
+					}],
+					"products":  [{
+						"product_id": "22435961",
+						"name": "Balcão",
+						"qty": "1",
+						"price": "1000",
+						"exemption_reason": "M01"
+						}]
+			}
+						}
+			
+			reque.post(options, function(error, response){
+				if(!error && response.statusCode == 200){
+					}
+				});			
+
+			response.redirect('/');
+
+		});
+
+
+router.get('/pagar', function(request, response) {
+		response.set("Content-Type", "text/html");
+		response.render('carrinho', {
+			errors: []
+		})
+	});
+				
+				
+router.post('/pagar', function(request, response) {
+				
+			
+	var options = {
+	url : 'http://localhost:8091/moloni/createSupplierReceipt',
+	method: 'POST',
+	dataType : 'json',
+	headers: headersOpt,
+	json: {
+        "company_id": "72407",
+        "date": "2019-04-09T17:31:05+0100",
+        "document_set_id": "123228",
+        "supplier_id": "533438",
+        "net_value": "1000",
+        "associated_documents": [{
+        	"associated_id": "0",
+        	"value": "1000"
+        }]
+}
+	}
+						
+	reque.post(options, function(error, response){
+	if(!error && response.statusCode == 200){
+		}
+	});			
+			
+	response.redirect('/');
 
 });
 
