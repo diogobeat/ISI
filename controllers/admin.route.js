4419074b5ed585a791f260d751c3a0036e868612
catiacunha <44875910+catiacunha@users.noreply.github.com>
2019-06-01 16:55:53 +0100

    Codigo Nav

--- controllers/admin.route.js
+++ controllers/admin.route.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const reque = require('request');
-
+const headersOpt = {  
+	"content-type": "application/json",
+};
 
 		 
 router.get('/',function(request,response){
@@ -39,6 +41,99 @@ router.get('/',function(request,response){
 router.post("/processOrder", function(request, response){
 	console.log(require('./public/jquery/teste'))
 });
+
+router.get('/fornecedorcomprarParafusos', function(request, response) {
+	response.set("Content-Type", "text/html");
+	response.render('admin/fornecedor', {
+		errors: []
+	})
+});
+	
+	
+router.post('/fornecedorcomprarParafusos', function(request, response) {
+	
+
+			var options = {
+				url : 'http://localhost:8091/dynamics/createOrderParafusos',
+				method: 'POST',
+				dataType : 'json',
+				headers: headersOpt,
+				json: {
+					"No": request.body.numeroOrder,
+					"Sell_to_Customer_No": "C00020",
+					"Sell_to_Customer_Name": "ISI 2019",
+					"Sell_to_Address": "Rua da Universidade do Minho",
+					"Sell_to_Post_Code": "4800-068",
+					"Sell_to_City": "Guimaraes",
+					"Sell_to_Contact_No": "CT000258",
+					"Sell_to_Contact": "Sr. Rui Vieria",
+					"Document_Date": "2019-05-24",
+					"Posting_Date": "2019-05-24",
+					"Order_Date": "2019-05-24",
+					"Due_Date": "2019-05-31",
+					"Type": "Item",
+					"No_Item": "70061",
+					"Quantity": "1000",
+					"Unit_of_Measure_Code": "PCS",
+					"Unit_of_Measure": "1",
+					"Unit_Price": "1"					
+			}
+						}
+			
+			reque.post(options, function(error, response){
+				if(!error && response.statusCode == 200){
+					}
+				});			
+
+			response.redirect('/admin/fornecedorcomprarParafusos');
+
+		});
+router.get('/fornecedorcomprarMadeira', function(request, response) {
+	response.set("Content-Type", "text/html");
+	response.render('admin/fornecedor', {
+		errors: []
+	})
+});
+			
+			
+router.post('/fornecedorcomprarMadeira', function(request, response) {
+	
+
+			var options = {
+				url : 'http://localhost:8091/dynamics/createOrderMadeira',
+				method: 'POST',
+				dataType : 'json',
+				headers: headersOpt,
+				json: {
+					"No": request.body.numeroOrder,
+					"Sell_to_Customer_No": "C00020",
+					"Sell_to_Customer_Name": "ISI 2019",
+					"Sell_to_Address": "Rua da Universidade do Minho",
+					"Sell_to_Post_Code": "4800-068",
+					"Sell_to_City": "Guimaraes",
+					"Sell_to_Contact_No": "CT000258",
+					"Sell_to_Contact": "Sr. Rui Vieria",
+					"Document_Date": "2019-05-24",
+					"Posting_Date": "2019-05-24",
+					"Order_Date": "2019-05-24",
+					"Due_Date": "2019-05-31",
+					"Type": "Item",
+					"No_Item": "70062",
+					"Quantity": "1000",
+					"Unit_of_Measure_Code": "PCS",
+					"Unit_of_Measure": "1",
+					"Unit_Price": "1"					
+			}
+						}
+			
+			reque.post(options, function(error, response){
+				if(!error && response.statusCode == 200){
+					}
+				});			
+
+			response.redirect('/admin/fornecedorcomprarMadeira');
+
+		});
 
 
 module.exports = router;
