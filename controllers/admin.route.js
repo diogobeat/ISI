9e4dc0a5864535f4cd64ed9d1c5cd101085422c6
NunoXavierFernandes <nunofxff@gmail.com>
2019-06-05 12:38:09 +0100

    coisas

--- controllers/admin.route.js
+++ controllers/admin.route.js
@@ -42,6 +42,43 @@ router.post("/processOrder", function(request, response){
 	console.log(require('./public/jquery/teste'))
 });
 
+// Fornecedores
+
+
+router.get('/fornecedorcomprarParafusos',function(request,response){
+	//console.log(request.user);
+	//console.log(request.isAuthenticated());
+		reque.get('http://localhost:8094/bitrix/getParafusos', function(error,response2,body){
+		/*	var earningsAnnual = 0;
+			var currentDate = new Date();
+			var year = currentDate.getFullYear().toString();
+			var month = (currentDate.getMonth() + 1).toString();
+*/
+			var bitrix = XMLDocument.parse(body);
+			var precoParafusos = bitrix.data.price;
+
+	/*		for(var i = 0 ; i < bitrix.data.length ; i++){
+				var saleM = bitrix.data[i].documentDate.substring(5, 7).replace(/^0+/, '');
+				var saleA = bitrix.data[i].documentDate.substring(0, 4);
+				//console.log(saleA  == year);
+				if(saleA  === year){
+					earningsAnnual +=  jasmin.data[i].amount;
+
+					if(saleM === month){
+						earningsMonthly += jasmin.data[i].amount;
+					}
+				}		
+			} */
+			response.set("Content-Type", "text/html");
+			response.render('admin/parafusos', {body: bitrix});
+		});
+	
+	
+});
+
+
+
+
 router.get('/fornecedorcomprarParafusos', function(request, response) {
 	response.set("Content-Type", "text/html");
 	response.render('admin/fornecedor', {
