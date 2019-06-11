c507c337697dfdc11b285f8b3c47de21ebd35a99
Tiago Pimenta Da Costa <tiagopimcosta@gmail.com>
2019-06-12 00:51:08 +0100

    FrontEnd

--- controllers/admin.route.js
+++ controllers/admin.route.js
@@ -44,6 +44,31 @@ router.get('/',function(request,response){
 });
 
 
+router.get('/vendas',function(request,response){
+	//console.log(request.user);
+	//console.log(request.isAuthenticated());
+		reque.get('http://localhost:8089/jasminapi/getOrder', function(error,response2,body){
+			var jasmin = JSON.parse(body);
+			
+			response.set("Content-Type", "text/html");
+			response.render('admin/vendas', {body: jasmin});
+		});
+	
+	
+});
+
+router.get('/compras',function(request,response){
+	//console.log(request.user);
+	//console.log(request.isAuthenticated());
+		reque.get('http://localhost:8090/jasminapi/getpurchasesorder', function(error,response2,body){
+			var jasmin = JSON.parse(body);
+			
+			response.set("Content-Type", "text/html");
+			response.render('admin/compras', {body: jasmin});
+		});
+	
+	
+});
 router.post("/processOrder", function(request, response){
 	console.log(require('./public/jquery/teste'))
 });
